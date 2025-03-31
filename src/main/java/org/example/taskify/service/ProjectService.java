931fc8f688e956fa9d37154f7fd7e9baf4dd3ddc
Deschna <pashkadit@gmail.com>
2025-03-31 14:23:26 +0300

    Set up pagination with sorting and configure Open API documentation

--- src/main/java/org/example/taskify/service/ProjectService.java
+++ src/main/java/org/example/taskify/service/ProjectService.java
@@ -1,15 +1,17 @@
 package org.example.taskify.service;
 
-import java.util.List;
 import org.example.taskify.dto.request.ProjectRequestDto;
 import org.example.taskify.dto.response.ProjectResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ProjectService {
+
     ProjectResponseDto initiateProject(ProjectRequestDto projectRequestDto);
 
     ProjectResponseDto getProjectById(Long id);
 
-    List<ProjectResponseDto> getAllProjects();
+    Page<ProjectResponseDto> getAllProjects(Pageable pageable);
 
     ProjectResponseDto updateProject(Long id, ProjectRequestDto projectRequestDto);
 
